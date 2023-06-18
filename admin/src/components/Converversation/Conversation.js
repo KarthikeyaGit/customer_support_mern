433183cec77a173e5b95dbbb02548782792d96f2
karthik61 <karthik@aspectsites.com>
2023-06-18 17:40:36 +0530

    chat window added in conversaton section

--- admin/src/components/Converversation/Conversation.js
+++ admin/src/components/Converversation/Conversation.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Conversation.scss';
 import arrowdown from '../../assets/arrow-down-.svg';
 
 
 function Conversation() {
-  const items = ['Item 1', 'Item 2', 'Item 3','Item 1', 'Item 2', 'Item 3','Item 1', 'Item 2', 'Item 3','Item 3','Item 1', 'Item 2', 'Item 3'];
+  let items = ['Item 1', 'Item 2', 'Item 3', 'Item 1', 'Item 2', 'Item 3', 'Item 1', 'Item 2', 'Item 3', 'Item 3', 'Item 1', 'Item 2', 'Item 3'];
   const renderItems = [];
 
+
+  const [messageInput, setMessageInput] = useState('');
+  const [ConvList , setConvList] = useState([{'from':'agent', 'to': 'user', 'message':'Hello'},{'from':'user', 'to': 'agent', 'message':'Hi'},{'from':'agent', 'to': 'user', 'message':'How are you?'}])
+
+  const handleInputChange = (e) => {
+    setMessageInput(e.target.value);
+  };
+
+  
+  const handleSend = () => {
+    if (messageInput.length > 0) {
+      setMessageInput('');
+      setConvList((prevConvList) => [
+        ...prevConvList,
+        { from: 'user', to: 'agent', message: messageInput }
+      ]);
+    }
+    
+  };
+  
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSend();
+    }
+  };
+
+  const selectedConvList = () => {
+    return (
+      <ul className="chat-list">
+        <li style={{ marginTop: 'auto' }}></li>
+        {ConvList.map((conv, index) => (
+          <li key={index}>
+            <div
+              className={`chat-bubble ${conv.from === 'agent' ? 'left' : 'right'}`}
+            >
+              {conv.message}
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+ 
   const listItems = () => {
     for (let i = 0; i < items.length; i++) {
       renderItems.push(
@@ -23,8 +67,8 @@ function Conversation() {
   }
 
   return (
-    <div className='d-flex flex-row conv'>
-        <div className='conv-container w-100'>
+    <div className='conv'>
+      <div className='conv-container'>
         <div className='conv-heading'>
           Conversation
         </div>
@@ -36,20 +80,43 @@ function Conversation() {
           <span className="conv-search-icon ms-2">&#128269;</span>
           <input type="text" className="form-control conv-search-input" placeholder="Search by conversation" />
         </div>
-        <div className="conv-list">
-          <div className='conv-list-header align-items-center'>
-            <img src={arrowdown} alt="arrowdown" style={{ height: '15px' }} />
-            <div className='ms-2 list-header'>Not Serviced</div>
-          </div>
-          <div className='conv-list-items'>
-            <ul>
-              {listItems()}
-            </ul>
+        <div className='conv-list-header align-items-center'>
+          <img src={arrowdown} alt="arrowdown" style={{ height: '15px' }} />
+          <div className='ms-2 list-header'>Not Serviced</div>
+        </div>
+        <div className='conv-list-items'>
+          <ul>
+            {listItems()}
+          </ul>
+        </div>
+      </div>
+      <div className='conv-chat-window'>
+        <div className='chat-window-topbar'>
+          <div className='topbar-avatar ms-3'>KK</div>
+          <div className='ms-3'>
+           Karthikeya
           </div>
         </div>
+        <div className='chat-window-body'>
+          {selectedConvList()}
         </div>
-
+        <div className='chat-window-input'>
+            <textarea
+              onKeyDown={handleKeyDown}
+              value={messageInput}
+              onChange={handleInputChange}
+              placeholder='Type your message...'
+            />
+          <button className='send-button btn btn-primary' onClick={handleSend}  style={{ height: 'fit-content' }}>
+              Send
+            </button>
+          </div>
+      </div>
+      {/* <div className='conv-user-info'>
+        
+      </div> */}
     </div>
+
   );
 }
 
